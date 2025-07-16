9e6ef5f5d6f404956c979140bbc2bf551a1264b5
Danyel <dpadayao40@gmail.com>
2025-07-16 15:48:12 +0800

    fix: invalid props

--- src/app/dashboard/project/[projectId]/page.tsx
+++ src/app/dashboard/project/[projectId]/page.tsx
@@ -1,12 +1,12 @@
 import { redirect, RedirectType } from "next/navigation";
 import React from "react";
 
-type Props = {
+type ProjectIdProps = {
   params: Promise<{ projectId: string }>;
   children: React.ReactNode;
 };
 
-const ProjectIdPage = async ({ params }: Props) => {
+const ProjectIdPage = async ({ params }: ProjectIdProps) => {
   const { projectId } = await params;
   return redirect(`${projectId}/overview`, RedirectType.replace);
 };
