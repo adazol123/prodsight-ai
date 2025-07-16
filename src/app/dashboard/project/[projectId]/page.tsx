eb5df7e770baea424afe152b7d265f60d44b67d7
Danyel <dpadayao40@gmail.com>
2025-07-16 15:56:41 +0800

    fix: invalid props & useSearchParams must wrapped in suspense

--- src/app/dashboard/project/[projectId]/page.tsx
+++ src/app/dashboard/project/[projectId]/page.tsx
@@ -1,14 +1,13 @@
 import { redirect, RedirectType } from "next/navigation";
-import React from "react";
 
 type ProjectIdProps = {
   params: Promise<{ projectId: string }>;
-  children: React.ReactNode;
 };
 
 const ProjectIdPage = async ({ params }: ProjectIdProps) => {
   const { projectId } = await params;
-  return redirect(`${projectId}/overview`, RedirectType.replace);
+  redirect(`${projectId}/overview`, RedirectType.replace);
+  return null;
 };
 
 export default ProjectIdPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,39 @@
-import { Toaster } from '@/components/shared/sonner'
-import TanstackProviders from '@/providers/tanstack.provider'
-import type { Metadata } from 'next'
-import { Geist_Mono, Outfit } from 'next/font/google'
-import '../styles/css/globals.css'
-import AnonymousAuthStatus from './_components/anonymous-auth-status'
-import HeaderSection from './_components/header-section'
-import LoginModal from './_components/login-modal'
-import ProjectModal from './_components/project-modal'
+import { Toaster } from "@/components/shared/sonner";
+import TanstackProviders from "@/providers/tanstack.provider";
+import type { Metadata } from "next";
+import { Geist_Mono, Outfit } from "next/font/google";
+import { Suspense } from "react";
+import "../styles/css/globals.css";
+import AnonymousAuthStatus from "./_components/anonymous-auth-status";
+import HeaderSection from "./_components/header-section";
+import LoginModal from "./_components/login-modal";
+import ProjectModal from "./_components/project-modal";
 const outfit = Outfit({
-  variable: '--font-outfit',
-  subsets: ['latin']
-})
+  variable: "--font-outfit",
+  subsets: ["latin"],
+});
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
-})
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Adazolhub | ProdSight AI',
+  title: "Adazolhub | ProdSight AI",
   description:
-    "ProdSight helps anyone with an idea turn it into a real product—faster and with less stress. Whether you're a developer, designer, or just getting started, our AI tools guide you every step of the way."
-}
+    "ProdSight helps anyone with an idea turn it into a real product—faster and with less stress. Whether you're a developer, designer, or just getting started, our AI tools guide you every step of the way.",
+};
 
-
-export default function RootLayout ({
-  children
+export default function RootLayout({
+  children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
         <script
-          src='https://challenges.cloudflare.com/turnstile/v0/api.js'
+          src="https://challenges.cloudflare.com/turnstile/v0/api.js"
           async
           defer
         ></script>
@@ -41,13 +41,13 @@ export default function RootLayout ({
       <body className={`${outfit.variable} ${geistMono.variable} antialiased`}>
         <HeaderSection />
         <AnonymousAuthStatus />
-        <TanstackProviders>
-        {children}
-        </TanstackProviders>
+        <TanstackProviders>{children}</TanstackProviders>
         <LoginModal />
-        <ProjectModal />
-        <Toaster position='top-right' expand />
+        <Suspense>
+          <ProjectModal />
+        </Suspense>
+        <Toaster position="top-right" expand />
       </body>
     </html>
-  )
+  );
 }
