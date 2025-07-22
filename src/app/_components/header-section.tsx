88ec5206bfdffe916bc1ddbf72baa8731c49fd3c
Danyel <dpadayao40@gmail.com>
2025-07-22 11:28:22 +0800

    refactor: replace local state to global session state

--- src/app/_components/header-section.tsx
+++ src/app/_components/header-section.tsx
@@ -1,49 +1,45 @@
-'use client'
-import { cn } from '@/lib/utils'
-import { layoutVariants } from '@/styles/variants/layout.variant'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import LoginButton from './login-button'
-import NewProjectButton from './new-project-button'
-import Logo from '../../assets/prodsight-logo-sm.svg'
-import { createClient } from '@/utils/supabase/client'
-import SignOutButton from '../login/_components/signout-button'
-import { Session } from '@supabase/supabase-js'
+"use client";
+import { cn } from "@/lib/utils";
+import { useAuthSessionStore } from "@/store/session-store";
+import { layoutVariants } from "@/styles/variants/layout.variant";
+import { createClient } from "@/utils/supabase/client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Logo from "../../assets/prodsight-logo-sm.svg";
+import SignOutButton from "../login/_components/signout-button";
+import LoginButton from "./login-button";
+import NewProjectButton from "./new-project-button";
 
 const HeaderSection = () => {
-  const supabase = createClient()
-  const [session, setSession] = React.useState<Session | null>(null)
-
+  const supabase = createClient();
+  const _session = useAuthSessionStore()._session;
+  const setAuthSession = useAuthSessionStore().setAuthSession;
   // call unsubscribe to remove the callback
   React.useEffect(() => {
     const { data } = supabase.auth.onAuthStateChange((event, session) => {
-      console.log(event, session)
-      setSession(session)
-    })
-    return () => data.subscription.unsubscribe()
-  }, [supabase.auth])
-
-  const isAnonymous = session?.user?.is_anonymous
-  const isDev = process.env.NODE_ENV !== 'production'
-  const isLoggedIn = !!session && !isAnonymous
-  // const userProfile = seson?.user?.user_metadata
+      setAuthSession(session);
+    });
+    return () => {
+      setAuthSession(null);
+      data.subscription.unsubscribe();
+    };
+  }, [supabase.auth, setAuthSession]);
 
   return (
-    <header className={cn(layoutVariants({ className: 'min-h-16 py-3' }))}>
-      <nav className='flex justify-between items-center gap-4'>
+    <header className={cn(layoutVariants({ className: "min-h-16 py-3" }))}>
+      <nav className="flex justify-between items-center gap-4">
         <Link
-          href='/'
-          className='flex items-center gap-2 select-none'
+          href="/"
+          className="flex items-center gap-2 select-none"
           tabIndex={0}
           replace
         >
-          <Image src={Logo} alt='ProdSight Logo' />
-          <span className='hidden sm:inline'>Adazolhub | ProdSight AI</span>
+          <Image src={Logo} alt="ProdSight Logo" />
+          <span className="hidden sm:inline">Adazolhub | ProdSight AI</span>
         </Link>
-        <div className='flex gap-2 items-center'>
-         {isDev && !!session?.user && <span className='text-xs text-muted-foreground inline-flex px-2 py-0.5 bg-muted rounded-3xl'>{session?.user?.id}</span>}
-          {isLoggedIn ? (
+        <div className="flex gap-2 items-center">
+          {!!_session && !_session?.user?.is_anonymous ? (
             <SignOutButton />
           ) : (
             <>
@@ -54,7 +50,7 @@ const HeaderSection = () => {
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default HeaderSection
+export default HeaderSection;
